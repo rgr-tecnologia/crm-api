598dc5fdadeeb4052e4822e0d9561bd2c358777a
@vcastro-dev <vcastro2395@gmail.com>
2024-01-03 14:03:59 -0300

    feat: adicionando propriedades na model Oportunidade

--- modules/oportunidades/dtos/oportunidade.dto.ts
+++ modules/oportunidades/dtos/oportunidade.dto.ts
@@ -1,9 +1,62 @@
 import { z } from "zod";
 
+// model Oportunidade {
+//   id                     String                     @id @default(uuid())
+//   clienteId              String
+//   cliente                Cliente                    @relation(fields: [clienteId], references: [id])
+//   representanteId        String
+//   representante          ClienteRepresentante       @relation(fields: [representanteId], references: [id])
+//   //TBD - departamento
+//   propostas              Proposta[]
+//   titulo                 String
+//   //TBD - gerenteComercial
+//   caracteristica         OportunidadeCaracteristica
+//   //TBD - periodoContratual
+//   //TBD - valorPrevisto
+//   areaExecutora          AreaExecutora
+//   dataFechamentoPrevista DateTime
+//   status                 OportunidadeStatus         @default(NEGOCIACAO)
+//   createdAt              DateTime                   @default(now())
+//   updatedAt              DateTime                   @default(now()) @updatedAt
+// }
+
+// enum OportunidadeStatus {
+//   NEGOCIACAO
+//   FECHADA
+//   PERDIDA
+// }
+
+// enum OportunidadeCaracteristica {
+//   MENSALIDADE
+//   PROJETO
+// }
+
+// enum AreaExecutora {
+//   INFRAESTRUTURA
+//   DESENVOLVIMENTO
+//   BUSINESS_INTELLIGENCE
+//   ALOCACAO_DE_RECURSOS
+//   HUNTING_DE_RECURSOS
+// }
+
 export const OportunidadeDto = z.object({
-  clienteId: z.string(),
-  representanteId: z.string(),
+  id: z.string().uuid(),
+  clienteId: z.string().uuid(),
+  representanteId: z.string().uuid(),
+  propostas: z.array(z.string().uuid()),
   titulo: z.string(),
+  caracteristica: z.enum(["MENSALIDADE", "PROJETO"]),
+  areaExecutora: z.enum([
+    "INFRAESTRUTURA",
+    "DESENVOLVIMENTO",
+    "BUSINESS_INTELLIGENCE",
+    "ALOCACAO_DE_RECURSOS",
+    "HUNTING_DE_RECURSOS",
+  ]),
+  dataFechamentoPrevista: z.string().datetime(),
+  status: z.enum(["NEGOCIACAO", "FECHADA", "PERDIDA"]),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
 
 export type ContratoDTO = z.infer<typeof OportunidadeDto>;
