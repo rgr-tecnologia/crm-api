aaa41fe33344782014353e916d273b98189c1390
@vcastro-dev <vcastro2395@gmail.com>
2024-03-17 18:09:44 -0300

    feat: implementando autorização

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as express from "express";
 import { router } from "@/modules/routes";
-import { validateJWT } from "./middlewares/checkJWT";
 
 const PORT = process.env.API_PORT;
 const app = express();
@@ -8,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1", validateJWT, router);
+app.use("/api/v1", router);
 
 app.use("/", (req, res, next) => {
   res.status(404).json({ message: "Not Found" });
--- src/middlewares/checkJWT.ts
+++ src/middlewares/checkJWT.ts
@@ -1,33 +1,7 @@
-import { NextFunction, Request, Response } from "express";
-import { verify } from "jsonwebtoken";
+import { auth } from "express-oauth2-jwt-bearer";
 
-export const validateJWT = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const token = req.headers.authorization;
-  if (!token) {
-    return res.status(403).json({ message: "Token is required" });
-  }
-
-  try {
-    const secret = process.env.AUTH0_CLIENT_SECRET;
-    const audience = process.env.AUTH0_AUDIENCE;
-    const issuer = process.env.AUTH0_ISSUER_BASE_URL;
-
-    if (!secret || !audience || !issuer) {
-      return res.status(500).json({ message: "Internal server error" });
-    }
-
-    verify(token, secret, {
-      audience,
-      issuer,
-      algorithms: ["HS256"],
-    });
-
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-};
+export const checkJwt = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  tokenSigningAlg: "RS256",
+});
